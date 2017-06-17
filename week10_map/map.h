8451e63e66066de043b89a04439dd0974f7baf59
Justin9four <justinchandler.17@gmail.com>
2017-06-17 15:28:14 -0600

    .

---
+++ week10_map/map.h
@@ -0,0 +1,41 @@
+/***********************************************************************
+* File: map.h
+************************************************************************/
+
+#ifndef MAP
+#define MAP
+
+#include <iostream> 
+#include "pair.h"
+/***********************************************
+* MAP
+* 
+**********************************************/
+template <class T>
+class Map
+{
+public:
+	// constructors
+
+	// destructor
+
+	// methods
+	// empty()
+	// size()
+	// clear()
+	// find()
+	// begin
+	// end()
+
+	// operator=
+	// operator[]
+
+	// members
+	// BST made from pair type
+
+};
+
+// nonmember function prototypes
+
+
+#endif // MAP
