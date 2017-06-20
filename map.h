6852d3752d5b44e4f29d20042beca2a93dcffe3c
Justin Chandler <justinchandler.17@gmail.com>
2017-06-20 09:50:24 -0600

    .

--- map.h
+++ map.h
@@ -19,23 +19,24 @@ class Map
 {
 public:
 	// constructors
-	Map() : BST() {}
+	Map() : {bst = new BST<Pair<K,V>>;}
 	// copy constructor
 
 	// destructor
-	~Map() {}
+	~Map() {bst->clear();}
 	// methods
-	// empty()
-	// size()
-	// clear()
-	// find()
-	// begin
-	// end()
+	bool empty() { return bst->empty(); }
+	int size() { return bst->empty() ? 0 : bst->root->size();}
+	void clear() { bst->clear(); }
+	RedBlackTreeNodeIterator <T> find(const T & t) {return bst->find(t);}
+	RedBlackTreeNodeIterator <T> begin() { return bst->begin();}
+	RedBlackTreeNodeIterator <T> end() { return bst->end();}
 	// operator=
 	// operator[]
+	
 	// members
 	// BST made from pair type
-    BST <Pair<K,V>> bst;
+    BST <Pair<K,V>> * bst;
 
 };
 
