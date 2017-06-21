d01ac94dd9bec8c557d95b6ca07932a67c98e066
nhbench <nhbench@gmail.com>
2017-06-21 16:12:28 -0400

    Update wordCount.h

--- wordCount.h
+++ wordCount.h
@@ -5,17 +5,21 @@
 *    This will contain just the prototype for the wordCount()
 *    function
 * Author
-*    <your names here>
+*    Nathan Bench, Jed Billman, Jeremy Chandler, Justin Chandler
 ************************************************************************/
 
 #ifndef WORD_COUNT_H
 #define WORD_COUNT_H
 #include <fstream>
 #include <iostream>
-#include <map>
+#include "map.h"
 #include <string>
 #include "pair.h"
 using namespace std;
+
+// forward declaration
+class Count;
+
 /*****************************************************
 * WORD COUNT
 * Prompt the user for a file to read, then prompt the
@@ -23,12 +27,25 @@ using namespace std;
 *****************************************************/
 void wordCount();
 string promptUser();
-void readFile(map <string, double> & counts,
-	string fileName,
-	const string & search);
-void display(string input, map<string, double>& counts);
+void readFile(Map <string, Count> & counts, string fileName);
+void display(string input, Map<string, Count>& counts);
 
-
-
+class Count
+{
+   public:
+      Count(): num(0) {}
+      
+      Count & operator += (const int & rhs)
+      {
+         num += rhs;
+         return *this;
+      }
+      
+      friend std::ostream & operator << (std::ostream & out, const Count & rhs);
+      
+      
+   private:
+      int num;
+};
 #endif // WORD_COUNT_H
 
